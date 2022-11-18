0ba0c9f6e4519d26c4bea137c774972c7b76d464
Nathan Lanza <nathanlanza@gmail.com>
2022-11-18 18:34:59 -0500

    [Lily] Format

--- Lily/main.swift
+++ Lily/main.swift
@@ -7,22 +7,21 @@
 
 import Foundation
 
-
 class Ty {
-  
+
 }
 
 class Value {
   let ty: Ty
   let definingOperation: Operation
   let name: String
-  
+
   static var nextDefaultName = 1
-  
+
   init(ty: Ty, definingOperation: Operation, name: String?) {
     self.ty = ty
     self.definingOperation = definingOperation
-    
+
     if let name = name {
       self.name = name
     } else {
@@ -37,7 +36,7 @@ class Region {
   init(basicBlocks: [BasicBlock] = []) {
     self.basicBlocks = basicBlocks
   }
-  
+
   func appendNewBasicBlock() -> BasicBlock {
     basicBlocks.append(BasicBlock())
     return basicBlocks.last!
@@ -59,9 +58,9 @@ class Operation {
   }
   var operands: [Value]
   var results: [Value]
-  
-  var attributes: [String:Attribute]
-  
+
+  var attributes: [String: Attribute]
+
   var regions: [Region]
 
   init() {
@@ -70,60 +69,62 @@ class Operation {
     self.attributes = [:]
     self.regions = []
   }
-  
+
   func print() {
     assert(results.count <= 1)
-    
+
     if !results.isEmpty {
       Swift.print("%\(results[0].name) = ", terminator: "")
     }
-    
+
     Swift.print("\(getName())", terminator: "")
-    
+
     if !operands.isEmpty {
       Swift.print(" (", terminator: "")
     }
-    
-    Swift.print(operands.map { "%\($0.name)" }.joined(separator: ", "), terminator: "")
-    
+
+    Swift.print(
+      operands.map { "%\($0.name)" }.joined(separator: ", "), terminator: "")
+
     if !operands.isEmpty {
       Swift.print(")", terminator: "")
     }
-    
+
     if !attributes.isEmpty {
       Swift.print(" {", terminator: "")
     }
-    
-    Swift.print(attributes.map({ element in
-      "\(element.key) : \(element.value)"
-    }).joined(separator: ", "), terminator: "")
-    
+
+    Swift.print(
+      attributes.map({ element in
+        "\(element.key) : \(element.value)"
+      }).joined(separator: ", "), terminator: "")
+
     if !attributes.isEmpty {
       Swift.print("}", terminator: "")
     }
-    
+
     if regions.isEmpty {
       return
     }
-    
+
     Swift.print(" {")
-    
+
     let bb = regions[0].basicBlocks[0]
-    
+
     for operation in bb.operations {
       Swift.print("  ", terminator: "")
       operation.print()
     }
-    
+
     Swift.print("")
     Swift.print("}")
-    
+
   }
 }
 
 class Function: Operation {
-  var insertionPoint: (Int, Int) = (0,0)
-  
+  var insertionPoint: (Int, Int) = (0, 0)
+
   func getFunctionName() -> String {
     return (attributes["functionName"] as! StringAttribute).value
   }
@@ -131,17 +132,17 @@ class Function: Operation {
   override func getName() -> String {
     return "func"
   }
-  
+
   func getBody() -> Region {
     return regions[0]
   }
-  
+
   init(name: String) {
     super.init()
     self.regions.append(Region())
     self.attributes["functionName"] = StringAttribute(value: name)
   }
-  
+
   func appendOp(_ op: Operation) {
     let bb = getBody().basicBlocks[insertionPoint.0]
     bb.operations.append(op)
@@ -153,12 +154,13 @@ class ConstantOp: Operation {
   override func getName() -> String {
     return "constant"
   }
-  
+
   init(value: Attribute, name: String? = nil) {
     super.init()
     attributes["value"] = value
 
-    results.append(Value(ty: value.getTy(), definingOperation: self, name: name))
+    results.append(
+      Value(ty: value.getTy(), definingOperation: self, name: name))
   }
 }
 
@@ -166,7 +168,7 @@ class Attribute: CustomDebugStringConvertible {
   func getTy() -> Ty {
     fatalError("Can't use base attribute")
   }
-  
+
   var debugDescription: String {
     fatalError("Can't use base attribute")
   }
@@ -187,7 +189,7 @@ class I64Attribute: Attribute {
   override func getTy() -> Ty {
     return I64Ty.get()
   }
-  
+
   override var debugDescription: String {
     return String(value)
   }
@@ -200,7 +202,7 @@ class StringAttribute: Attribute {
   override func getTy() -> Ty {
     return StringTy.get()
   }
-  
+
   override var debugDescription: String {
     return "\"\(value)\""
   }
