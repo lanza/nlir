ff813c9d0f4ece4b1b823f9c0202a5753f6fd994
Nathan Lanza <nathanlanza@gmail.com>
2022-11-18 21:05:11 -0500

    [Lily] Add Traits for OneResult and OneTypedResult

---
+++ Lily/Traits.swift
@@ -0,0 +1,27 @@
+//
+//  Traits.swift
+//  Lily
+//
+//  Created by Nathan Lanza on 11/18/22.
+//
+
+import Foundation
+
+protocol OneResult: Operation {
+  func getResult() -> Value
+}
+
+extension OneResult {
+  func getResult() -> Value {
+    results.first!
+  }
+}
+
+protocol OneTypedResult: Operation {
+  func getType() -> Ty
+}
+extension OneTypedResult {
+  func getType() -> Ty {
+    return results.first!.getTy()
+  }
+}
