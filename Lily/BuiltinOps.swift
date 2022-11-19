3ffa3396c0e84a7604d043f98876852652865d12
Nathan Lanza <nathanlanza@gmail.com>
2022-11-18 21:52:12 -0500

    [Lily] Move the builtin ops to their own file

---
+++ Lily/BuiltinOps.swift
@@ -0,0 +1,67 @@
+//
+//  BuiltinOps.swift
+//  Lily
+//
+//  Created by Nathan Lanza on 11/18/22.
+//
+
+import Foundation
+
+class Function: Operation {
+  var insertionPoint: (Int, Int) = (0, 0)
+
+  func getFunctionName() -> String {
+    return (attributes["functionName"] as! StringAttribute).value
+  }
+
+  override func getName() -> String {
+    return "func"
+  }
+
+  func getBody() -> Region {
+    return regions[0]
+  }
+
+  init(name: String, type: FunctionTy) {
+    super.init()
+    self.regions.append(Region())
+    self.attributes["functionName"] = StringAttribute(value: name)
+    self.attributes["functionType"] = TypeAttribute(type: type)
+  }
+
+  func appendOp(_ op: Operation) {
+    let bb = getBody().basicBlocks[insertionPoint.0]
+    bb.operations.append(op)
+    insertionPoint.1 = bb.operations.count
+  }
+}
+
+class ConstantOp: Operation, OneTypedResult {
+  override func getName() -> String {
+    return "constant"
+  }
+
+  init(value: Attribute, name: String? = nil) {
+    super.init()
+    attributes["value"] = value
+
+    results.append(
+      Value(ty: value.getTy(), definingOperation: self, name: name))
+  }
+}
+
+class AddOp: Operation, OneTypedResult {
+  override func getName() -> String {
+    return "add"
+  }
+
+  init(lhs: Value, rhs: Value) {
+    super.init()
+
+    assert(lhs.getTy() === rhs.getTy())
+
+    operands.append(lhs)
+    operands.append(rhs)
+    results.append(Value(ty: lhs.getTy(), definingOperation: self))
+  }
+}
